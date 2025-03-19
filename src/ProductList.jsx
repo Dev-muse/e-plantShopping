8455cf765623aead22d36d6199dca4f008200744
Rahman <museabdirahman@gmail.com>
2025-03-19 18:16:23 +0000

    update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -293,7 +293,7 @@ function ProductList({ onHomeClick }) {
     setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
     setShowCart(false); // Hide the cart when navigating to About Us
   };
-
+ 
   const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
